ba7982563ecc04c27b747aca7d515d23d1db99cb
Muhammad Harris <harris20014@gmail.com>
2022-09-17 20:01:35 +0500

    Update v1.0.0
    - Application Development Completed

---
+++ Application/assets/javascript/main.js
@@ -0,0 +1,31 @@
+document.querySelector("button").addEventListener('click', function() {
+    var input = document.querySelector("input").value;
+
+    var api_key = "X21mpSOykjswkRD7fetIw8Mj00fH0OaT";
+    var api_url = "http://api.giphy.com/v1/gifs/search?";  
+
+    var url = api_url + "q=" + input + "&api_key=" + api_key;
+    console.log(url);
+
+    // AJAX Request
+    var GiphyAJAXCall = new XMLHttpRequest();
+    GiphyAJAXCall.open( 'GET', url );
+    GiphyAJAXCall.send()
+
+    GiphyAJAXCall.addEventListener('load', function(e){
+        var data = e.target.response;
+        PushToMain(data);
+    });
+});
+
+function PushToMain(input) {
+    var response = JSON.parse(input);
+    var imageUrls = response.data;
+
+    imageUrls.forEach(function(image){
+        var src = image.images.fixed_height.url;
+        var container = document.querySelector(".gif-container");
+        console.log(src);
+        container.innerHTML += "<img src=\"" + src + "\" class=\"image-container\">";
+    });
+}
